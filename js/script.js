a3480266d86cf113a3489ad6e2cc6be866b57183
Daniele <danielekesia@outlook.com>
2023-04-04 14:44:08 +0100

    create albums released section carousel

--- js/script.js
+++ js/script.js
@@ -42,3 +42,51 @@ bannerSlider.addEventListener('transitionend', () => {
     bannerSlider.style.transition = 'all .5s';
   });
 });
+
+// Albums released carousel
+// Dragging controls
+const albumsCarousel = document.querySelector('.albums-carousel');
+
+let isDragStart = false,
+  prevPageX, prevScrollLeft;
+
+const dragStart = (e) => {
+  // updating global variables value on mouse down left
+  isDragStart = true;
+  prevPageX = e.pageX;
+  prevScrollLeft = albumsCarousel.scrollLeft;
+}
+
+const dragStop = (e) => {
+  isDragStart = false;
+}
+
+const dragging = (e) => {
+  // scrolling cards to left according to mouse pointer
+  if (!isDragStart) return;
+  e.preventDefault();
+  let positionDiff = e.pageX - prevPageX;
+  albumsCarousel.scrollLeft = prevScrollLeft - positionDiff;
+}
+
+albumsCarousel.addEventListener('mousedown', dragStart);
+albumsCarousel.addEventListener('mousemove', dragging);
+albumsCarousel.addEventListener('mouseup', dragStop);
+
+// Arrows controls
+const productContainers = [...document.querySelectorAll('.albums-carousel')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+  let containerDimensions = item.getBoundingClientRect();
+  let containerWidth = containerDimensions.width;
+
+  nxtBtn[i].addEventListener('click', () => {
+    item.scrollLeft += containerWidth;
+  })
+
+  preBtn[i].addEventListener('click', () => {
+    item.scrollLeft -= containerWidth;
+  })
+})
